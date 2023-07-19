d45cdd45ccd6c1639a60c6130b20051514cde76d
Dilshoda Abdualimova <dilshoda@dilshodas-mbp.mynetworksettings.com>
2023-07-18 23:00:51 -0400

    feat: stopwatch

--- src/app/users/stopwatch/page.js
+++ src/app/users/stopwatch/page.js
@@ -1,16 +1,39 @@
 "use client"
 // import css
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Stopwatch() {
     const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [running]);
 
     return (
         <>
             <h1> Task Stopwatch</h1>
             <div>
-                <span>{("0" + Math.floor((time / 60000) % 60))}</span>
-
+                <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+            </div>
+            <div>
+                {running ? (
+                    <button onClick={() => { setRunning(false) }}>Stop</button>
+                ) : (
+                    <button onClick={() => { setRunning(true) }}>Start</button>
+                )
+                }
+                <button onClick={() => { setTime(0) }}>Reset</button>
             </div>
         </>
     );
