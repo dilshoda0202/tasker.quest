b515778f429629128ba5228bc549d02690eed853
rufuscodes <rufuscodes@gmail.com>
2023-07-22 09:27:42 -0700

    add: task-2 updated to resemble events-2

--- src/app/users/tasks-2/page.js
+++ src/app/users/tasks-2/page.js
@@ -1,27 +1,34 @@
 'use client';
 import { useEffect, useState } from 'react';
-import TaskTable from './taskTable';
+import TaskTable from './TaskTable';
 
 export default function FilterableTaskTable() {
+  // state is what the data is representing in realtime
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/tasks`)
+    fetch(`${ process.env.NEXT_PUBLIC_SERVER_URL }/tasks`)
       .then((res) => res.json())
       .then((newData) => {
+        // console.log('Can you see newData?', newData)
         setData(newData);
+        // console.log(data)
         setLoading(false);
+
       });
 
   }, []);
 
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No data shown...</p>
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No data shown...</p>;
 
   return (
     <main>
       <TaskTable tasks={data.tasks} />
     </main>
+
+
+
   );
 }
